ec0d3d85f845c1da83ae94b10a24de5e5ac8b032
Shad3 <122724277+LONELY-DUckWolf@users.noreply.github.com>
2025-05-14 23:02:08 +0300

    ъуъ

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import products from "./base-data/products.json"
+import createList from "./templates/products.hbs"
+console.log(createList({products}))
+console.log("test")
+const body = document.querySelector("body")
+body.innerHTML = createList(products);
